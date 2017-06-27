30192b1808a92315d92dbf00ae5e82b444d3e1d3
Samuel Sapalski <dev@sapalski.de>
2017-06-27 22:14:03 +0200

    gui: add TextViewer
    The TextViewer class provides a generic way to create a text window
    across different skins which embeds itself into the current skin.
    
    This class is also compatible with older Kodi versions.

---
+++ resources/lib/audioaddict/gui.py
@@ -0,0 +1,16 @@
+import xbmcgui
+
+
+class TextViewer(xbmcgui.WindowXMLDialog):
+    def __new__(cls, *args, **kwargs):
+        return super(TextViewer, cls).__new__(cls,
+                                              "DialogTextViewer.xml",
+                                              None)
+
+    def __init__(self, header, text):
+        self.header = header
+        self.text = text
+
+    def onInit(self):
+        self.getControl(1).setLabel(self.header)
+        self.getControl(5).setText(self.text)
