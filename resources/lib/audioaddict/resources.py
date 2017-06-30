7f833cc7df9b2644bdebd64c5693fd5c9619e77b
Samuel Sapalski <dev@sapalski.de>
2017-06-30 22:37:44 +0200

    resources: generalize path functions
    Introduce new path functions:
      * get_addon_path
      * get_data_path
    
    The intention is to use these as a base for all static data under
    resources/data. More static data is to be expected, this change
    prepares easier integration of those resources.
    
    Changes got reflected with this change as well in function
    get_welcome_text.

--- resources/lib/audioaddict/resources.py
+++ resources/lib/audioaddict/resources.py
@@ -2,15 +2,23 @@ import os
 import xbmc
 
 
-def get_welcome_path(addon):
+def get_addon_path(addon):
     kodi_path = addon.getAddonInfo('path')
     real_path = xbmc.translatePath(kodi_path).decode('utf-8')
 
-    return os.path.join(real_path, 'resources', 'data', 'welcome.txt')
+    return real_path
+
+
+def get_data_path(addon):
+    addon_path = get_addon_path(addon)
+    data_path = os.path.join(addon_path, 'resources', 'data')
+
+    return data_path
 
 
 def get_welcome_text(addon):
-    welcome_path = get_welcome_path(addon)
+    data_path = get_data_path(addon)
+    welcome_text_path = os.path.join(data_path, 'welcome.txt')
 
-    with open(welcome_path, 'r') as f:
+    with open(welcome_text_path, 'r') as f:
         return f.read()
