602a8a37fe27fbb910619abdd550039e89eb6e31
Samuel Sapalski <dev@sapalski.de>
2017-07-02 22:18:03 +0200

    addon: refactor parse_args
    parse_args has now a more descriptive name => parse_addon_args.
    The semantics have slightly changed:
      * arguments of multiple defined parameters are parsed to lists
      * removes leading '?'
      * doesn't throw ValueError anymore
    
    Additionally added documentation for this function.

--- addon.py
+++ addon.py
@@ -6,17 +6,52 @@ sys.path.insert(0, os.path.join(sys.path[0], 'resources', 'lib'))
 import audioaddict
 
 
-def parse_args(query_string):
-    args = urlparse.parse_qs(query_string)
+def parse_addon_args(query_string):
+    """Parse addon arguments.
+
+    Parse arguments given by Kodi to the addon call. This function
+    expects a 'query string' to be parsed and returns a dictionary.
+
+    Multiple appearances of parameters are parsed to lists in the
+    order how they have appeared, otherwise they are parsed to plain
+    values.
+
+    A leading '?' sign will get removed if present. This makes it
+    possible to pass sys.argv[2] directly to this function.
+
+    All arguments are parsed to strings.
+
+    Note:
+        Invalid input is silently ignored, see example three.
+
+    Args:
+        query_string (str): the 'query string' to be parsed.
+
+    Examples:
+        >>> parse_args("param1=arg1&param2=arg2")
+        {'param1': 'arg1', 'param2': 'arg2'}
+
+        >>> parse_args("param1=arg1&param1=arg2")
+        {'param1': ['arg1', 'arg2']}
+
+        >>> parse_args("param1=arg1&param2")
+        {'param1': 'arg1'}
+
+    Returns:
+        dict: the parsed 'query string'.
+
+    """
+    if query_string.startswith('?'):
+        args = urlparse.parse_qs(query_string[1:])
+    else:
+        args = urlparse.parse_qs(query_string)
 
     if not args:
         return {}
 
     for key, values in args.iteritems():
-        if len(values) > 1:
-            raise ValueError("multiple values for '%s' not allowed" % key)
-
-        args[key] = values[0]
+        if len(values) == 1:
+            args[key] = values[0]
 
     return args
 
@@ -24,7 +59,7 @@ def parse_args(query_string):
 def main():
     addon_url = sys.argv[0]
     addon_handle = int(sys.argv[1])
-    addon_args = parse_args(sys.argv[2][1:])
+    addon_args = parse_addon_args(sys.argv[2])
 
     audioaddict.run_addon(addon_url, addon_handle, addon_args)
 
