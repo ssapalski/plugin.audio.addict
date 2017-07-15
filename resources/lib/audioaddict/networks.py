765d2a5d5bc90ae71a284f050b04233db4716907
Samuel Sapalski <dev@sapalski.de>
2017-07-15 10:40:47 +0200

    networks: support new listen_key handling
    Remove credential checks and authentication, they have been moved
    to the main/play module. Make sure listen_key won't be forwarded
    anymore.

--- resources/lib/audioaddict/networks.py
+++ resources/lib/audioaddict/networks.py
@@ -2,33 +2,10 @@ import xbmcgui
 import xbmcplugin
 
 from audioaddict.api import AudioAddictApi
-from audioaddict.exceptions import EmptyCredentialsError, \
-                                   NoNetworksSelectedError
-
-
-def get_credentials(addon):
-    username = addon.getSetting('username')
-    password = addon.getSetting('password')
-
-    if not username and not password:
-        raise EmptyCredentialsError()
-
-    return username, password
-
-
-def get_listen_key(addon, settings):
-    username, password = get_credentials(addon)
-    network = settings.networks[0]
-
-    api = AudioAddictApi(network.key)
-    response = api.authenticate(username, password)
-
-    return response['listen_key']
+from audioaddict.exceptions import NoNetworksSelectedError
 
 
 def show_networks(addon, settings):
-    listen_key = get_listen_key(addon, settings)
-
     succeeded = False
     for network in settings.networks:
         if not addon.getBooleanSetting('activate_%s' % network.key):
@@ -38,8 +15,7 @@ def show_networks(addon, settings):
 
         url_parameters = {
             'mode': 'open_network',
-            'network_key': network.key,
-            'listen_key': listen_key
+            'network_key': network.key
         }
 
         list_item = xbmcgui.ListItem(label=network.display_name)
