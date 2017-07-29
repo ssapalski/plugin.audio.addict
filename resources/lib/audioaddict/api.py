8ead70fb94f6d08e6e00637e6a603df1bba04284
Samuel Sapalski <dev@sapalski.de>
2017-07-29 14:25:50 +0200

    api: introduce Channel/Channels class
    These classes wrap the JSON API response for the retrieved channels.
    The purpose is to aggregate channel specific functionality into
    these classes instead of having them spread across several
    functions.
    
    Furthermore they provide documentation about the JSON API responses
    of the AudioAddict API without explicitly writing docstrings.

--- resources/lib/audioaddict/api.py
+++ resources/lib/audioaddict/api.py
@@ -27,12 +27,9 @@ class AudioAddictApi(object):
         r = requests.get("http://%s/channels" % self._base_url)
         r.raise_for_status()
 
-        channels = []
-        for channel in r.json():
-            if not channel['name'].startswith('X'):
-                channels.append(channel)
+        channels = _create_channel_list(r.json())
 
-        return sorted(channels, key=lambda channel: channel['key'])
+        return Channels(channels)
 
     def playlist(self, stream_key, channel_key, listen_key):
         r = requests.get("https://%s/listen/%s/%s?listen_key=%s" %
@@ -53,3 +50,44 @@ class User(object):
     @property
     def listen_key(self):
         return self._user['listen_key']
+
+
+class Channels(object):
+    def __init__(self, channels):
+        self._channels = channels
+
+    def supported(self):
+        return [x for x in self._channels if x.supported()]
+
+
+class Channel(object):
+    def __init__(self, parsed_json):
+        self._channel = parsed_json
+
+    def image_default(self):
+        url = "http:%s" % self._channel['images']['default']
+        url = url.split('{')[0]
+
+        return url
+
+    def supported(self):
+        if self.name.startswith('X'):
+            return False
+
+        return True
+
+    @property
+    def key(self):
+        return self._channel['key']
+
+    @property
+    def name(self):
+        return self._channel['name']
+
+
+def _create_channel_list(parsed_json):
+    channels = []
+    for channel in parsed_json:
+        channels.append(Channel(channel))
+
+    return channels
--- resources/lib/audioaddict/channels.py
+++ resources/lib/audioaddict/channels.py
@@ -3,17 +3,9 @@ import xbmcplugin
 from audioaddict.api import AudioAddictApi
 
 
-def build_icon_url(channel):
-    icon_url = "http:%s" % channel['images']['default']
-    icon_url = icon_url.split('{')[0]
-
-    return icon_url
-
-
 def build_list_item(channel):
-    icon_url = build_icon_url(channel)
-    list_item = xbmcgui.ListItem(label=channel['name'],
-                                 thumbnailImage=icon_url)
+    list_item = xbmcgui.ListItem(label=channel.name,
+                                 thumbnailImage=channel.image_default())
 
     list_item.setProperty('isPlayable', 'true')
 
@@ -31,11 +23,12 @@ def show_channels(addon, settings):
     network = settings.get_network(addon.args['network_key'])
     channels = get_channels(network)
 
-    for channel in channels:
+    supported_channels = channels.supported()
+    for channel in supported_channels:
         url_parameters = {
             'mode': 'play_stream',
             'network_key': network.key,
-            'channel_key': channel['key']
+            'channel_key': channel.key
         }
 
         stream_url = addon.buildUrl(url_parameters)
@@ -45,6 +38,6 @@ def show_channels(addon, settings):
                                     url=stream_url,
                                     listitem=list_item,
                                     isFolder=False,
-                                    totalItems=len(channels))
+                                    totalItems=len(supported_channels))
 
     xbmcplugin.endOfDirectory(handle=addon.handle, succeeded=True)
